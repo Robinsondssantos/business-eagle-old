c7695189c3efb4a9a40adef3335b13667536814d
Robinson D. S. Santos <robinsondssantos@gmail.com>
2020-03-11 12:01:09 -0300

    improve cards

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="container">
-    <h1>Main</h1>
+    <div class="balance-card">
+      <span class="balance-label">Balance</span>
+      <span class="balance-value">R$ 100,00</span>
+    </div>
+    <div class="income-card">
+      <span class="income-label">Income</span>
+      <span class="income-value">R$ 150,00</span>
+    </div>
+    <div class="expense-card">
+      <span class="expense-label">Expense</span>
+      <span class="expense-value">R$ 50,00</span>
+    </div>
   </div>
 </template>
 
@@ -25,6 +36,60 @@ export default {
     .container {
       margin: 80px 20px;
     }
+  }
+
+  .balance-card {
+    display: flex;
+    flex-direction: column;
+    /* justify-content: space-between; */
+    /* align-items: center; */
+    border-bottom: 1px solid #eee;
+    padding: 20px 0 20px 0;
+    /* font-size: 18px; */
+  }
+
+  .balance-label {
+    color: #777;
+  }
+
+  .balance-value {
+    font-size: 32px;
+    font-weight: 800;
+    color: #008800 ;
+    padding: 10px 0 0 0;
+  }
+
+  .income-card {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    border-bottom: 1px solid #eee;
+    padding: 20px 0 20px 0;
+  }
+
+  .income-label {
+    color: #777;
+  }
+
+  .income-value {
+    color: #0000ff;
+  }
+
+  .expense-card {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    border-bottom: 1px solid #eee;
+    padding: 20px 0 20px 0;
+    margin-bottom: 20px;
+  }
+
+  .expense-label {
+    color: #777;
+  }
+
+  .expense-value {
+    color: #ff0000;
   }
 
 </style>
