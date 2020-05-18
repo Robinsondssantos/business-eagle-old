751c84688416b0401412f7f5bd1de94e31a1ea60
Robinson D. S. Santos <robinsondssantos@gmail.com>
2020-05-18 13:42:16 -0300

    improve scaffold

--- express-api/src/server.js
+++ express-api/src/server.js
@@ -1,9 +1,12 @@
-import express from 'express';
+// import express from 'express';
 
-// const express = require('express')
-const app = express()
-const port = 3000
+// const app = express()
+// const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
+// app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
+
+import App from './App';
+
+App.listen();
