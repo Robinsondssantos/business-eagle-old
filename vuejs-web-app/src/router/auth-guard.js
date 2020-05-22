8b8d7a32edbfc237dc7382b4d5424fc482698e5b
Robinson D. S. Santos <robinsondssantos@gmail.com>
2020-05-22 10:36:59 -0300

    fix navigation using auth-guard

--- vuejs-web-app/src/router/auth-guard.js
+++ vuejs-web-app/src/router/auth-guard.js
@@ -1,1 +1,10 @@
+import store from '../store';
 
+export default (to, from, next) => {
+
+  if (store.getters.isLoggedIn) {
+    return next()
+  } else {
+    return next(`/signin?redirect=${to.path}`)
+  }
+}
--- vuejs-web-app/src/router/index.js
+++ vuejs-web-app/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Main from '../views/Main.vue'
+import authGuard from './auth-guard'
 
 Vue.use(VueRouter)
 
@@ -8,53 +8,60 @@ const routes = [
   {
     path: '/',
     name: 'Main',
-    component: Main
-  },
-  {
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: () => import('../views/Dashboard.vue'),
-  },
-  {
-    path: '/incomes',
-    name: 'Incomes',
-    component: () => import('../views/Incomes.vue'),
-    props: true
-  },
-  {
-    path: '/expenses',
-    name: 'Expenses',
-    component: () => import('../views/Expenses.vue'),
-    props: true
-  },
-  {
-    path: '/expense_types',
-    name: 'ExpenseTypes',
-    component: () => import('../views/ExpenseTypes.vue'),
-    props: true
-  },  
+    component: () => import('../views/Main.vue'),
+    beforeEnter: authGuard,
+    children: [
+      {
+        path: '/dashboard',
+        name: 'Dashboard',
+        component: () => import('../views/Dashboard.vue'),
+      },
+      {
+        path: '/incomes',
+        name: 'Incomes',
+        component: () => import('../views/Incomes.vue'),
+        props: true,
+      },
+      {
+        path: '/expenses',
+        name: 'Expenses',
+        component: () => import('../views/Expenses.vue'),
+        props: true,
+      },
+      {
+        path: '/expense_types',
+        name: 'ExpenseTypes',
+        component: () => import('../views/ExpenseTypes.vue'),
+        props: true,
+      },
+      {
+        path: '/expensesSummary',
+        name: 'ExpensesSummary',
+        component: () => import('../views/ExpensesSummary.vue')
+      },
+      {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+      },      
+    ]
+  }, 
   {
     path: '/signin',
     name: 'Signin',
-    component: () => import('../views/Signin.vue')
+    component: () => import('../views/Signin.vue'),
   },
   {
     path: '/signup',
     name: 'Signup',
     component: () => import('../views/Signup.vue')
-  },  
-  {
-    path: '/expensesSummary',
-    name: 'ExpensesSummary',
-    component: () => import('../views/ExpensesSummary.vue')
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '*',
+    redirect: '/'
   }
 ]
 
