26f566559adf272ef1462566837fc0982a8e9eb5
Robinson D. S. Santos <robinsondssantos@gmail.com>
2020-05-27 14:10:38 -0300

    add ExpenseType and IncomeType in routes

--- express-api/src/routes.js
+++ express-api/src/routes.js
@@ -2,10 +2,21 @@ import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
+import ExpenseTypeController from './app/controllers/'
+
+import authMiddleware from './app/middlewares/auth';
+import customHeaders from './app/middlewares/customHeader';
 
 const routes = new Router();
 
+routes.use(customHeaders);
+
 routes.post('/session', SessionController.store);
 routes.post('/users', UserController.store);
+
+routes.use(authMiddleware);
+
+routes.post('expense_types', );
+routes.post('income_types', );
 
 export default routes;
