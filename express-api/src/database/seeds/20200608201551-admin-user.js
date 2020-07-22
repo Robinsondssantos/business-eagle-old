0b7365d00b1eab06912ef8725563261e6a41c61c
Robinson D. S. Santos <robinsondssantos@gmail.com>
2020-07-22 14:28:41 -0300

    change email

--- express-api/src/database/seeds/20200608201551-admin-user.js
+++ express-api/src/database/seeds/20200608201551-admin-user.js
@@ -8,7 +8,7 @@ module.exports = {
             first_name: 'Robinson',
             last_name: 'D. S. Santos',
             phone: '554632256575',
-            email: 'robinsondssantos@gmail.com.br',
+            email: 'robinsondssantos@gmail.com',
             password_hash: '$2a$08$K1GfNUBZ0Nvo.bp1Jhb60uF7nXJeRGDKrbzHx2hejA.i2MkUl/fIi', // MLHRqLYxPev2zSF8     
             created_at: currentDate,
             updated_at: currentDate,          
