b3722ed16c783f5b2afee32514ee515dbadef707
Robinson D. S. Santos <robinsondssantos@gmail.com>
2020-08-11 21:41:18 -0300

    add vuetify version

---
+++ vuetify-app-starter/src/views/ContasAReceber.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container
+    fluid
+    grid-list-lg
+  >
+    <v-layout row justify-center>
+      <v-flex xs12 sm9 md9 lg6 mt-12>
+        <v-card>
+          <v-card-title>
+            <v-spacer></v-spacer>
+            <v-btn depressed small>Adicionar</v-btn>
+          </v-card-title>
+          <v-card-text>
+            <v-data-table>
+            </v-data-table>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
---
+++ vuetify-app-starter/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <app-container></app-container>
+</template>
+
+<script>
+// @ is an alias to /src
+import Container from '@/components/Container.vue'
+
+export default {
+  name: 'home',
+  components: {
+    'app-container': Container
+  }
+}
+</script>
